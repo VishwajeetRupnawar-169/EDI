27d86a4fbaa16f601d52c89c48f1381e0b19989d
VishwajeetRupnawar-169 <142874925+VishwajeetRupnawar-169@users.noreply.github.com>
2024-07-29 10:54:00 +0530

    Delete backend directory

--- backend/models/Description.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const descriptionSchema = new mongoose.Schema({
-  content: String
-});
-
-const Description = mongoose.model('Description', descriptionSchema);
-
-module.exports = Description;
